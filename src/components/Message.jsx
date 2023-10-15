5ef18bf29ad269d314352cdb208503fefa3df877
nisoojadhav <nisoojadhav@gmail.com>
2023-10-15 11:37:18 +0530

    minor error solved

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,11 @@
 import React, { useContext, useRef, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
+import CryptoJS from "crypto-js";
 
 const Message = ({ message }) => {
+  const decryptedText = CryptoJS.AES.decrypt(message.text, "my-secret-key");
+  const originalText = decryptedText.toString(CryptoJS.enc.Utf8);
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
 
@@ -13,26 +16,30 @@ const Message = ({ message }) => {
   }, [message]);
 
   return (
-    <div
-      ref={ref}
-      className={`message ${message.senderId === currentUser.uid && "owner"}`}
-    >
-      <div className="messageInfo">
-        <img
-          src={
-            message.senderId === currentUser.uid
-              ? currentUser.photoURL
-              : data.user.photoURL
-          }
-          alt=""
-        />
-        <span>just now</span>
+    <>
+      <div
+        ref={ref}
+        className={`message ${
+          message.senderId === currentUser.uid ? "owner" : ""
+        }`}
+      >
+        <div className="messageInfo">
+          <img
+            src={
+              message.senderId === currentUser.uid
+                ? currentUser.photoURL
+                : data.user.photoURL
+            }
+            alt=""
+          />
+          <span>just now</span>
+        </div>
+        <div className="messageContent">
+          <p>{originalText}</p>
+          {message.img && <img src={message.img} alt="" />}
+        </div>
       </div>
-      <div className="messageContent">
-        <p>{message.text}</p>
-        {message.img && <img src={message.img} alt="" />}
-      </div>
-    </div>
+    </>
   );
 };
 
